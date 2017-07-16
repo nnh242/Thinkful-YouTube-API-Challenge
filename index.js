935bbeddfdcac66bbd6553715de2f6d5578a9a98
nnh242 <nhoang@me.com>
2017-07-15 23:02:00 -0400

    added video title, channel and styling

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getYouTubeApi(searchTerm, callback) {
         key: MY_API_KEY,
         q: searchTerm,
         type :'video',
-        maxResults: 5
+        maxResults: 6
     }
     $.getJSON(YOUTUBE_SEARCH_URL, params, callback);
 }
@@ -17,11 +17,19 @@ function getYouTubeApi(searchTerm, callback) {
 function displayResults(resultObject) {
     $('#resultsContainer').empty();
     let items = resultObject.items;
+    console.log(items);
     for (let i=0; i<items.length; i++){
     $('#resultsContainer').append(
-        `<div class="d-inline-block" id="video">
-        <a href='https://www.youtube.com/watch?v=${items[i].id.videoId}'><img src="${items[i].snippet.thumbnails.default.url}"></a>
-        </div>`);
+        `<div class="row">
+            <div class=".col-xs-6" id="video">
+                <a href='https://www.youtube.com/watch?v=${items[i].id.videoId}'><img src="${items[i].snippet.thumbnails.high.url}"></a>
+            </div>
+            <div class=".col-xs-6">
+                <div class=".col-xs-6"><span>${items[i].snippet.title}</span></div>
+                <div class=".col-xs-6"><span>${items[i].snippet.channelTitle}</span></div>
+            </div>
+        </div>
+        `);
     }
 }
 
